10fa75157e52923d11cd3b193219c60ca72a1ca9
Tinar Saun <tinartinar720@gmail.com>
2023-12-19 18:13:43 +0700

    tambahin

---
+++ app/Http/Controllers/api/TransaksiWalletController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Deposit;
+use App\Models\TransaksiWallet;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class DepositController extends Controller
+{
+    public function showAll()
+    {
+        $deposit = TransaksiWallet::all();
+
+        return response([
+            'message' => 'All Deposit Retrieved',
+            'data' => $deposit
+        ], 200);
+    }
+
+    public function showById($id)
+    {
+        $transaksiWallet = TransaksiWallet::where('id_transaksi_wallet' , $id);
+
+        return response([
+            'message' => 'Show Transaksi Wallet Successfully',
+            'data' => $transaksiWallet
+        ], 200);
+    }
+
+    public function showByTypeTransaksi($type)
+    {
+        $transaksiWallet = TransaksiWallet::where('id_type_transaksi' , $type);
+
+        return response([
+            'message' => 'Show Transaksi Wallet Successfully',
+            'data' => $transaksiWallet
+        ], 200);
+    }
+
+    public function showByIdUser($id)
+    {
+        $transaksiWallet = TransaksiWallet::where('id_user' , auth()->id())->get();
+
+        return response([
+            'message' => 'Show Transaksi Wallet Successfully',
+            'data' => $transaksiWallet
+        ], 200);
+    }
+
+    public function deposit(Request $request)
+    {
+        $data = $request->all();
+
+        $validate = Validator::make($data, [
+            'jumlah' => 'required',
+            'metode_pembayaran' => 'required',
+        ]);
+
+        $user = User::find(auth()->id());
+
+        if ($validate->fails()) {
+            return response(['message' => $validate->errors()->first()], 400);
+        }
+
+        $data['tanggal'] = Carbon::now()->format('d-m-y');
+        $data['status'] = 'success';
+        $data['type_transaksi'] = 'deposit';
+        $data['id_user'] = auth()->id();
+        $deposit = TransaksiWallet::create($data);
+        $user['saldo']+=$deposit['jumlah'];
+        $user->save();
+
+        return response([
+            'message' => 'Transaksi Success',
+            'data' => $deposit
+        ], 200);
+    }
+
+    public function pembayaran(Request $request)
+    {
+        $data = $request->all();
+
+        $validate = Validator::make($data, [
+            'jumlah_deposit' => 'required',
+        ]);
+
+        $user = User::find(auth()->id());
+
+        if ($validate->fails()) {
+            return response(['message' => $validate->errors()->first()], 400);
+        }
+
+        $data['tanggal'] = Carbon::now()->format('d-m-y');
+        $data['status'] = 'success';
+        $data['metode_pembayaran'] = 'wallet';
+        $data['type_transaksi'] = 'pembayaran';
+        $data['id_user'] = auth()->id();
+        $bayar = TransaksiWallet::create($data);
+        $user['saldo']-=$bayar['jumlah'];
+        $user->save();
+
+        return response([
+            'message' => 'Transaksi Success',
+            'data' => $bayar
+        ], 200);
+    }
+
+}
--- app/Models/TransaksiWallet.php
+++ app/Models/TransaksiWallet.php
@@ -4,18 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Deposit extends Model
+class TransaksiWallet extends Model
 {
     protected $table = 'Transaksi_Wallet';
-    protected $primaryKey = 'id_deposit';
+    protected $primaryKey = 'id_transaksi_wallet';
     public $timestamps = false;
 
     protected $fillable = [
         'id_user',
-        
-        'jumlah_deposit',
+        'type_teransaksi',
+        'jumlah',
         'metode_pembayaran',
         'status',
-        'tanggal_deposit',
+        'tanggal',
     ];
 }
